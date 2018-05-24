56974fd3aa400d840f240d5fa1e03f2399906ae4
Vladimir Zotov <zott14@outlook.com>
2018-05-24 10:28:58 +0300

    Add PlayRecord

---
+++ Play/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayRecord 
+{
+    class Player : IPlayable, IRecordable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Play melody.");
+        }
+
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Pause melody.");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Stop melody.");
+        }
+
+        public void Record()
+        {
+            Console.WriteLine("Start recording.");
+        }
+
+        void IRecordable.Pause()
+        {
+            Console.WriteLine("Pause recording.");
+        }
+
+        void IRecordable.Stop()
+        {
+            Console.WriteLine("Stop recording.");
+        }
+    }
+}
---
+++ Play/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayRecord
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            player.Play();
+            (player as IPlayable).Pause();
+
+            player.Record();
+            (player as IRecordable).Stop();
+
+            Console.ReadKey();
+        }
+    }
+}
